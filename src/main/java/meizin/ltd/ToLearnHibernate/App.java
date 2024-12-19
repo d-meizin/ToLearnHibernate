64a7131573d093fbb585f85ff12d7993d4b6a0d5
d-meizin <d.meizin@gmail.com>
2024-12-19 12:07:15 +0300

    Initialise with Maven archetype

---
+++ src/main/java/meizin/ltd/ToLearnHibernate/App.java
@@ -0,0 +1,10 @@
+package meizin.ltd.ToLearnHibernate;
+
+/**
+ * for simple check 
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("meizin is working!");
+    }
+}
